086d65b5134cad984daf022a6f1c4462f09707a6
AshDavid12 <gaashdavid@gmail.com>
2024-08-29 15:02:12 +0300

    changed infer.py

--- infer.py
+++ infer.py
@@ -3,18 +3,20 @@ import runpod
 import base64
 import faster_whisper
 import tempfile
+from whisper_online import *
 
 import torch
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model_name = 'ivrit-ai/faster-whisper-v2-d3-e3'
-model = faster_whisper.WhisperModel(model_name, device=device)
-
+# model = faster_whisper.WhisperModel(model_name, device=device)
+model = FasterWhisperASR(model_name, modelsize=None, cache_dir=None, model_dir=None)
 import requests
 
 # Maximum data size: 200MB
 MAX_PAYLOAD_SIZE = 200 * 1024 * 1024
+
 
 def download_file(url, max_size_bytes, output_filename, api_key=None):
     """
@@ -41,7 +43,7 @@ def download_file(url, max_size_bytes, output_filename, api_key=None):
 
         # Get the file size if possible
         file_size = int(response.headers.get('Content-Length', 0))
-        
+
         if file_size > max_size_bytes:
             print(f"File size ({file_size} bytes) exceeds the maximum allowed size ({max_size_bytes} bytes).")
             return False
@@ -63,13 +65,14 @@ def download_file(url, max_size_bytes, output_filename, api_key=None):
         print(f"Error downloading file: {e}")
         return False
 
+
 def transcribe(job):
     datatype = job['input'].get('type', None)
     if not datatype:
-        return { "error" : "datatype field not provided. Should be 'blob' or 'url'." }
+        return {"error": "datatype field not provided. Should be 'blob' or 'url'."}
 
     if not datatype in ['blob', 'url']:
-        return { "error" : f"datatype should be 'blob' or 'url', but is {datatype} instead." }
+        return {"error": f"datatype should be 'blob' or 'url', but is {datatype} instead."}
 
     # Get the API key from the job input
     api_key = job['input'].get('api_key', None)
@@ -79,31 +82,60 @@ def transcribe(job):
 
         if datatype == 'blob':
             mp3_bytes = base64.b64decode(job['input']['data'])
-            open(audio_file, 'wb').write(mp3_bytes) 
+            open(audio_file, 'wb').write(mp3_bytes)
         elif datatype == 'url':
             success = download_file(job['input']['url'], MAX_PAYLOAD_SIZE, audio_file, api_key)
             if not success:
-                return { "error" : f"Error downloading data from {job['input']['url']}" }
-        
+                return {"error": f"Error downloading data from {job['input']['url']}"}
+
         result = transcribe_core(audio_file)
-        return { 'result' : result }
+        return {'result': result}
+
+
+def transcribe_whisper_streaming(job):
+    datatype = job['input'].get('type', None)
+    if not datatype:
+        return {"error": "datatype field not provided. Should be 'blob' or 'url'."}
+
+    if not datatype in ['blob', 'url']:
+        return {"error": f"datatype should be 'blob' or 'url', but is {datatype} instead."}
+
+    # Get the API key from the job input
+    api_key = job['input'].get('api_key', None)
+
+    with tempfile.TemporaryDirectory() as d:
+        audio_file = f'{d}/audio.mp3'
+
+        if datatype == 'blob':
+            mp3_bytes = base64.b64decode(job['input']['data'])
+            open(audio_file, 'wb').write(mp3_bytes)
+        elif datatype == 'url':
+            success = download_file(job['input']['url'], MAX_PAYLOAD_SIZE, audio_file, api_key)
+            if not success:
+                return {"error": f"Error downloading data from {job['input']['url']}"}
+
+        result = transcribe_core(audio_file)
+        return {'result': result}
+
 
 def transcribe_core(audio_file):
     print('Transcribing...')
 
-    ret = { 'segments' : [] }
+    ret = {'segments': []}
 
-    segs, dummy = model.transcribe(audio_file, language='he', word_timestamps=True)
+    segs, dummy = model.transcribe(audio_file, init_prompt="")
     for s in segs:
         words = []
         for w in s.words:
-            words.append( { 'start' : w.start, 'end' : w.end, 'word' : w.word, 'probability' : w.probability } )
+            words.append({'start': w.start, 'end': w.end, 'word': w.word, 'probability': w.probability})
 
-        seg = { 'id' : s.id, 'seek' : s.seek, 'start' : s.start, 'end' : s.end, 'text' : s.text, 'avg_logprob' : s.avg_logprob, 'compression_ratio' : s.compression_ratio, 'no_speech_prob' : s.no_speech_prob, 'words' : words }
+        seg = {'id': s.id, 'seek': s.seek, 'start': s.start, 'end': s.end, 'text': s.text, 'avg_logprob': s.avg_logprob,
+               'compression_ratio': s.compression_ratio, 'no_speech_prob': s.no_speech_prob, 'words': words}
 
         print(seg)
         ret['segments'].append(seg)
 
     return ret
 
-runpod.serverless.start({"handler": transcribe})
+
+runpod.serverless.start({"handler": transcribe_whisper_streaming})
