b4aefb4a3f1d44656bbd46cddabd2817f92df41e
AshDavid12 <gaashdavid@gmail.com>
2024-09-01 17:46:38 +0300

    updated to cuda 2.3.1,,12

--- infer.py
+++ infer.py
@@ -21,11 +21,7 @@ except Exception as e:
     logging.error(f"Unknown from exception- error to import whisper_online: {e}",exc_info=True)
 
 if torch.cuda.is_available():
-    num_gpus = torch.cuda.device_count()
-    logging.info(f"CUDA is available. Number of GPUs: {num_gpus}")
-    for i in range(num_gpus):
-        gpu_name = torch.cuda.get_device_name(i)
-        logging.info(f"GPU {i}: {gpu_name}")
+    logging.info(f"CUDA is available.")
 else:
     logging.info("CUDA is not available. Using CPU.")
 
